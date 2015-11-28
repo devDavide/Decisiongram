23a4e4293c0f6d636e382b6df1cadab834ff3b3f
devDavide <davide.pallaoro@gmail.com>
2015-11-28 20:59:49 +0100

    Added JUNIT tests

--- TMessagesProj/src/test/java/org/pollgram/decision/service/MessagesManagerTest.java
+++ TMessagesProj/src/test/java/org/pollgram/decision/service/MessagesManagerTest.java
@@ -38,26 +38,30 @@ public class MessagesManagerTest {
     private Map<Long, String> receivedId2LastMessages = new HashMap<>();
     private TLRPC.Chat chat;
     private TLRPC.User user;
+    private Decision decision;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         ApplicationLoader.applicationContext = mMockContext;
 
         chat = new TLRPC.Chat();
-        chat.id =39379118;
+        chat.id = 39379118;
 
         user = new TLRPC.User();
-        user.id = 22;
+        user.id = 93880097;
 
-        this.dao = new PollgramDAOTestImpl(chat.id);
+        this.dao = new PollgramDAOTestImpl(chat.id, user.id);
         messageManager = new PollgramMessagesManagerImpl(dao);
-        this.service = new PollgramServiceImpl(dao,messageManager);
+        this.service = new PollgramServiceImpl(dao, messageManager);
+
+        decision = dao.getDecisions(chat.id, null).get(0);
+
     }
 
     @Test
-    public void testMessageTypeDifferentEmoji(){
-        for (PollgramMessagesManager.MessageType mt1 : PollgramMessagesManager.MessageType.values()){
-            for (PollgramMessagesManager.MessageType mt2 : PollgramMessagesManager.MessageType.values()){
+    public void testMessageTypeDifferentEmoji() {
+        for (PollgramMessagesManager.MessageType mt1 : PollgramMessagesManager.MessageType.values()) {
+            for (PollgramMessagesManager.MessageType mt2 : PollgramMessagesManager.MessageType.values()) {
                 if (mt1.equals(mt2))
                     Assert.assertTrue(mt1.getEmoji().equals(mt2.getEmoji()));
                 else
@@ -67,42 +71,82 @@ public class MessagesManagerTest {
     }
 
     @Test
-    public void testMessageTypeGetEmoji(){
-        for (PollgramMessagesManager.MessageType mt1 : PollgramMessagesManager.MessageType.values()){
+    public void testMessageTypeGetEmoji() {
+        for (PollgramMessagesManager.MessageType mt1 : PollgramMessagesManager.MessageType.values()) {
             Assert.assertEquals(mt1, PollgramMessagesManager.MessageType.byEmoji(mt1.getEmoji()));
         }
     }
 
     @Test
     public void testSendVotes() throws ParseException {
-        Decision decision = dao.getDecisions(null).get(0);
         List<Option> options = dao.getOptions(decision);
         Collection<Vote> votes = new ArrayList<>();
         Date voteDate = new Date();
-        for (int i=0; i< options.size() ; i++ ){
-            Vote v = new Vote( i % 2 == 0, voteDate, user.id, options.get(i).getId());
+        for (int i = 0; i < options.size(); i++) {
+            Vote v = new Vote(i % 2 == 0, voteDate, user.id, options.get(i).getId());
             votes.add(v);
         }
         String message = messageManager.buildNotifyVoteMessage(decision, votes);
         PollgramMessagesManager.MessageType type = messageManager.getMessageType(message);
         Assert.assertEquals(type, PollgramMessagesManager.MessageType.VOTE);
 
-        Collection<Vote> parsedVotes = messageManager.getVotes(message, chat, voteDate, user.id);
-        Assert.assertEquals(parsedVotes,votes);
+        assertVotes(votes, voteDate, message);
+        assertVotes(votes, voteDate, messageManager.reformatMessage(message));
+    }
 
-        message = messageManager.reformatMessage(message);
-        parsedVotes = messageManager.getVotes(message, chat, voteDate, user.id);
-        Assert.assertEquals(parsedVotes,votes);
+    private void assertVotes(Collection<Vote> votes, Date voteDate, String message) {
+        Collection<Vote> parsedVotes = messageManager.getVotes(message, chat, voteDate, user.id);
+        Assert.assertEquals(parsedVotes, votes);
     }
 
     @Test
     public void testSendRemind() throws ParseException {
-        Decision decision = dao.getDecisions(null).get(0);
-
         String message = messageManager.buildRemindMessage("UserName String", decision);
         PollgramMessagesManager.MessageType type = messageManager.getMessageType(message);
         Assert.assertEquals(type, PollgramMessagesManager.MessageType.REMIND_TO_VOTE);
     }
 
+    @Test
+    public void testNewDecision() throws ParseException {
+        List<Option> options = dao.getOptions(decision);
+        String message = messageManager.buildNotifyNewDecision(decision, options);
+
+        PollgramMessagesManager.MessageType type = messageManager.getMessageType(message);
+        Assert.assertEquals(type, PollgramMessagesManager.MessageType.NEW_DECISION);
+
+        assertNewDecision(decision, options, message);
+        assertNewDecision(decision, options, messageManager.reformatMessage(message));
+
+    }
+
+    private void assertNewDecision(Decision decision, List<Option> options, String message) {
+        PollgramMessagesManager.NewDecisionData result = messageManager.getNewDecision(message, chat, user.id);
+        Assert.assertEquals(decision, result.decision);
+        Assert.assertEquals(options, result.optionList);
+    }
+
+    @Test
+    public void testCloseDecision() {
+        Option winningOption = dao.getOptions(decision).get(0);
+        int voteCount = 5;
+        String message = messageManager.buildCloseDecision(decision, winningOption, voteCount);
+        Assert.assertEquals(messageManager.getMessageType(message), PollgramMessagesManager.MessageType.CLOSE_DECISION);
+
+        assertCloseDecision(decision, winningOption, message);
+        assertCloseDecision(decision, winningOption, messageManager.reformatMessage(message));
+    }
+
+    private void assertCloseDecision(Decision decision, Option winningOption, String message) {
+        PollgramMessagesManager.ClosedDecisionDate result = messageManager.getCloseDecision(message, chat);
+        Assert.assertEquals(decision, result.decision);
+        Assert.assertEquals(winningOption, result.winningOption);
+    }
+
+    @Test
+    public void testReopenDecision() {
+        String message = messageManager.buildReopenDecision(decision);
+
+
+    }
 
 }
